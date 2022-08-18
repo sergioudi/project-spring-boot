8a8f21ed2708e55e493b3f119ac76d27b948978b
Sergio Ricardo de Sousa <sergio.ricardo.sousa@avanade.com>
2022-08-18 14:42:57 -0300

    feat(domain): create class Category and CategoryTest is ok

---
+++ domain/src/main/java/com/github/sergioudi/domain/category/Category.java
@@ -0,0 +1,70 @@
+package com.github.sergioudi.domain.category;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Category  {
+    private String ID;
+    private String name;
+    private String description;
+    private boolean active;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    private Category(
+            final String anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreationDate,
+            final Instant aUpdateDate,
+            final Instant aDeleteDate
+    ) {
+        this.ID = anId;
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = Objects.requireNonNull(aCreationDate, "'createdAt' should not be null");
+        this.updatedAt = Objects.requireNonNull(aUpdateDate, "'updatedAt' should not be null");
+        this.deletedAt = aDeleteDate;
+    }
+
+    public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
+        final var id = UUID.randomUUID().toString().toLowerCase();
+        final var now = Instant.now();
+        final var deletedAt = isActive ? null : now;
+        return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
+    }
+
+
+    public String getId() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+}
