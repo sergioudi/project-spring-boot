e2533384af2b76d74ced46a3ba87a0b6a83e0ab2
Sergio Ricardo de Sousa <sergio.ricardo.sousa@avanade.com>
2022-08-24 15:21:47 -0300

    feat: create tests in the application layer

--- application/src/main/java/com/github/sergioudi/application/video/create/DefaultCreateVideoUseCase.java
+++ application/src/main/java/com/github/sergioudi/application/video/create/DefaultCreateVideoUseCase.java
@@ -1,15 +1,20 @@
 package com.github.sergioudi.application.video.create;
 
+import com.github.sergioudi.domain.category.CategoryGateway;
 import com.github.sergioudi.domain.category.CategoryID;
 import com.github.sergioudi.domain.genre.GenreGateway;
 import com.github.sergioudi.domain.genre.GenreID;
+import com.github.sergioudi.domain.validation.Error;
+import com.github.sergioudi.domain.validation.ValidationHandler;
 import com.github.sergioudi.domain.video.Video;
 import com.github.sergioudi.domain.video.VideoGateway;
 import com.github.sergioudi.domain.validation.handler.Notification;
 import io.vavr.control.Either;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static io.vavr.API.Left;
 import static io.vavr.API.Try;
@@ -18,11 +23,13 @@ public class DefaultCreateVideoUseCase extends CreateVideoUseCase {
 
     private final VideoGateway videoGateway;
     private final GenreGateway genreGateway;
+    private final CategoryGateway categoryGateway;
 
     public DefaultCreateVideoUseCase(final GenreGateway genreGateway,
-                                     final VideoGateway videoGateway) {
+                                     final VideoGateway videoGateway, CategoryGateway categoryGateway) {
         this.videoGateway = Objects.requireNonNull(videoGateway);
         this.genreGateway = Objects.requireNonNull(genreGateway);
+        this.categoryGateway = categoryGateway;
     }
 
     @Override
@@ -35,6 +42,8 @@ public class DefaultCreateVideoUseCase extends CreateVideoUseCase {
         final var isActive = aCommand.isActive();
 
         final var notification = Notification.create();
+        notification.append(validateGenres(genres));
+        notification.append(validateCategory(aCategoryId));
 
         final var aVideo = Video.newVideo(aName, aDescription, aImdb, CategoryID.from(aCategoryId), isActive);
         aVideo.validate(notification);
@@ -42,6 +51,44 @@ public class DefaultCreateVideoUseCase extends CreateVideoUseCase {
         aVideo.addGenres(genres);
 
         return notification.hasError() ? Left(notification) : create(aVideo);
+    }
+
+    private ValidationHandler validateGenres(final List<GenreID> ids) {
+        final var notification = Notification.create();
+        if (ids == null || ids.isEmpty()) {
+            return notification;
+        }
+
+        final var retrievedIds = genreGateway.existsByIds(ids);
+
+        if (ids.size() != retrievedIds.size()) {
+            final var missingIds = new ArrayList<>(ids);
+            missingIds.removeAll(retrievedIds);
+
+            final var missingIdsMessage = missingIds.stream()
+                    .map(GenreID::getValue)
+                    .collect(Collectors.joining(", "));
+
+            notification.append(new Error("Some genres could not be found: %s".formatted(missingIdsMessage)));
+        }
+
+        return notification;
+    }
+
+    private ValidationHandler validateCategory(final String id) {
+        final var notification = Notification.create();
+        if (id == null) {
+            return notification;
+        }
+
+        final var retrievedId = categoryGateway.findById(CategoryID.from(id));
+
+        if (!retrievedId.isPresent()) {
+
+            notification.append(new Error("Category not be found: %s".formatted(id)));
+        }
+
+        return notification;
     }
 
     private Either<Notification, CreateVideoOutput> create(final Video aVideo) {
--- application/src/main/java/com/github/sergioudi/application/video/update/DefaultUpdateVideoUseCase.java
+++ application/src/main/java/com/github/sergioudi/application/video/update/DefaultUpdateVideoUseCase.java
@@ -1,8 +1,11 @@
 package com.github.sergioudi.application.video.update;
 
+import com.github.sergioudi.domain.category.CategoryGateway;
 import com.github.sergioudi.domain.category.CategoryID;
 import com.github.sergioudi.domain.genre.GenreGateway;
 import com.github.sergioudi.domain.genre.GenreID;
+import com.github.sergioudi.domain.validation.Error;
+import com.github.sergioudi.domain.validation.ValidationHandler;
 import com.github.sergioudi.domain.video.Video;
 import com.github.sergioudi.domain.video.VideoGateway;
 import com.github.sergioudi.domain.video.VideoID;
@@ -11,9 +14,11 @@ import com.github.sergioudi.domain.exceptions.NotFoundException;
 import com.github.sergioudi.domain.validation.handler.Notification;
 import io.vavr.control.Either;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import static io.vavr.API.Left;
 import static io.vavr.API.Try;
@@ -23,10 +28,13 @@ public class DefaultUpdateVideoUseCase extends UpdateVideoUseCase {
     private final VideoGateway videoGateway;
     private final GenreGateway genreGateway;
 
+    private final CategoryGateway categoryGateway;
+
     public DefaultUpdateVideoUseCase(final GenreGateway genreGateway,
-                                     final VideoGateway videoGateway) {
+                                     final VideoGateway videoGateway, CategoryGateway categoryGateway) {
         this.videoGateway = Objects.requireNonNull(videoGateway);
         this.genreGateway = Objects.requireNonNull(genreGateway);
+        this.categoryGateway = categoryGateway;
     }
 
     @Override
@@ -43,11 +51,52 @@ public class DefaultUpdateVideoUseCase extends UpdateVideoUseCase {
                 .orElseThrow(notFound(anId));
 
         final var notification = Notification.create();
+        notification.append(validateGenres(genres));
+        notification.append(validateCategory(aCategoryId));
+
         aVideo
                 .update(aName, aDescription, aImdb, CategoryID.from(aCategoryId), genres, isActive)
                 .validate(notification);
 
         return notification.hasError() ? Left(notification) : update(aVideo);
+    }
+
+    private ValidationHandler validateGenres(final List<GenreID> ids) {
+        final var notification = Notification.create();
+        if (ids == null || ids.isEmpty()) {
+            return notification;
+        }
+
+        final var retrievedIds = genreGateway.existsByIds(ids);
+
+        if (ids.size() != retrievedIds.size()) {
+            final var missingIds = new ArrayList<>(ids);
+            missingIds.removeAll(retrievedIds);
+
+            final var missingIdsMessage = missingIds.stream()
+                    .map(GenreID::getValue)
+                    .collect(Collectors.joining(", "));
+
+            notification.append(new Error("Some genres could not be found: %s".formatted(missingIdsMessage)));
+        }
+
+        return notification;
+    }
+
+    private ValidationHandler validateCategory(final String id) {
+        final var notification = Notification.create();
+        if (id == null) {
+            return notification;
+        }
+
+        final var retrievedId = categoryGateway.findById(CategoryID.from(id));
+
+        if (!retrievedId.isPresent()) {
+
+            notification.append(new Error("Category not be found: %s".formatted(id)));
+        }
+
+        return notification;
     }
 
     private Either<Notification, UpdateVideoOutput> update(final Video aVideo) {
